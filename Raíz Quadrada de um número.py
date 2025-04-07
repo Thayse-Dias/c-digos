bd55203ebfffa034a043abafb5bfd251f97582d9
Thayse-Dias <156327575+Thayse-Dias@users.noreply.github.com>
2025-04-07 13:21:33 -0300

    Add files via upload

---
+++ Raíz Quadrada de um número.py
@@ -0,0 +1,34 @@
+#Raíz Quadrada
+
+import math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A  raíz quadrada de {} é igual a {}.'.format(num,raiz))
+
+print('-'*15)
+
+from math import sqrt
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print('A  raíz quadrada de {} é igual a {}.'.format(num,raiz))
+
+print('-'*15)
+
+from math import sqrt
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print('A  raíz quadrada de {} é igual a {:.2f}.'.format(num,raiz))
+
+print('-'*15)
+
+import math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A  raíz quadrada de {} é igual a {}.'.format(num, math.ceil(raiz)))
+
+print('-'*15)
+
+import math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A  raíz quadrada de {} é igual a {}.'.format(num, math.floor(raiz)))
